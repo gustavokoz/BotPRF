93139274e7cdabc53e9bd4c195524198f290f5f9
gustavokoz <gustavokozbr@gmail.com>
2023-05-01 21:00:23 -0400

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 const config = process.env
-import {Client,GatewayIntentBits,Partials,Events,EmbedBuilder} from "discord.js"
+import {Client,GatewayIntentBits,Partials,Events,EmbedBuilder,TextChannel} from "discord.js"
 const client = new Client({
     intents: [
       GatewayIntentBits.Guilds,
@@ -22,12 +22,13 @@ const client = new Client({
     ]
   });
 client.on(Events.VoiceStateUpdate,(old,neww)=>{
-    if(old.member?.user.id == client.user.id) return;
-    const channel = old.guild.channels.cache.find(
+    if(old.member?.user.id == client?.user?.id) return;
+    const channel = old?.guild?.channels?.cache?.find(
         channel => channel.id === "1101639223313961051"
-      )
+      ) as TextChannel
       const embed = new EmbedBuilder()
-      .setDescription(old.channelId?neww.channelId?`<@${old.member.id}>Saiu da call <#${old.channelId}> Entrou na call <#${neww.channelId}>`:`<@${old.member.id}> Saiu da call <#${old.channelId}>`:`<@${old.member.id}> Entrou na call <#${neww.channelId}>`)
-      channel.send({embeds:[embed]})
+      .setDescription(old.channelId?neww.channelId?`<@${old?.member?.id}>Saiu da call <#${old.channelId}> Entrou na call <#${neww.channelId}>`:`<@${old?.member?.id}> Saiu da call <#${old.channelId}>`:`<@${old?.member?.id}> Entrou na call <#${neww.channelId}>`)
+      
+      channel?.send({embeds:[embed]})
 })
 client.login(config.token)
